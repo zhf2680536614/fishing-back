698ed1938f7d300338266931d21bd5333b9c6a06
zhf2680536614 <zhf160814..tey>
2026-03-03 21:09:58 +0800

    build: 初始化Maven项目基础结构
    添加Maven包装器脚本、配置文件及基础项目结构

---
+++ src/main/java/com/fishing/FishingApplication.java
@@ -0,0 +1,13 @@
+package com.fishing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FishingApplication.class, args);
+    }
+
+}
